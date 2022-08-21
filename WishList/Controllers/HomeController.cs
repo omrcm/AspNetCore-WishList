9dc4f0437187bcb23130ecde6aec983d56656887
Ömür Uçum <omurucum@gmail.com>
2022-08-21 16:17:21 +0300

    Update HomeController.cs

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -13,12 +13,12 @@ namespace WishList.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View("/Home/Index");
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return View("/Shared/Error");
+            return View("Error");
         }
     }
 }
