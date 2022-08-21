9225af2b2b1dd515e0973ba0a794a75ea99a7871
Ömür Uçum <omurucum@gmail.com>
2022-08-21 17:57:27 +0300

    Create ItemController's Delete Action

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context; 
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var list = _context.Items;
+            return View("Index",list);
+        }
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Creata");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            var data = new Item { Id = item.Id, Description = item.Description };
+            _context.Add(data);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.Local.First(c => c.Id == Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
